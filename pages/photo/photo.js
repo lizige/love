c5bcb4563e3145fd204633a86c0c5fdaf26d2442
lizige <306717465@qq.com>
2018-05-11 08:56:38 +0800

    add photos

---
+++ pages/photo/photo.js
@@ -0,0 +1,71 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+      imgs:[
+        'http://wx2.sinaimg.cn/mw690/adf1fab3gy1fr73l8o5ygj205k07fdfw.jpg',
+        "http://wx2.sinaimg.cn/mw690/adf1fab3gy1fr73lmvkkrj20u0140ag9.jpg",
+        "http://wx2.sinaimg.cn/mw690/adf1fab3gy1fr73li4fgrj20u01hcadc.jpg",
+        "http://wx3.sinaimg.cn/mw690/adf1fab3gy1fr73lefph0j20p90zitb7.jpg"
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
